482c3c95f9c1db66ed1605467b5f8165b3efcf1b
Michael Millward <michaelmillward@Michaels-MacBook-Pro.local>
2020-06-29 15:13:33 +1200

    apiversion issue

--- src/Services/Note/Note.API/Infrastructure/NoteContext.cs
+++ src/Services/Note/Note.API/Infrastructure/NoteContext.cs
@@ -7,7 +7,9 @@ namespace Note.API.Infrastructure
 {
     public class NoteContext : DbContext
     {
-        public DbSet<Notebook> Notebooks { get; set; } 
+        public const string DEFAULT_SCHEMA = "note";
+
+        public DbSet<Entry> Notebooks { get; set; } 
 
         public NoteContext(DbContextOptions<NoteContext> options) : base(options)
         {
